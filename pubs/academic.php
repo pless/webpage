2c8dc4be8b1ae7d181a66ca87da18487b834e4b4
Robert Pless <robert.pless@gmail.com>
2017-06-21 12:44:06 -0400

    first commit

---
+++ pubs/academic.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> 
+<html> 
+<head> 
+  <meta content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type"> 
+  <title>Robert Pless - Publications</title> 
+<link href="style.css" rel="stylesheet" type="text/css"> 
+<link href="bibtexbrowser.css" rel="stylesheet" type="text/css"> 
+</head> 
+<body> 
+ 
+<?php 
+   //$_GET['bib']='plessRefs.bib'; 
+   //$_GET['author']='Pless';
+   //$_GET['tag']='isomap';
+   //include( 'bibtexbrowser.php' ); 
+$_GET['bib']='plessRefs.bib'; 
+//$_GET['all']='isomap'; 
+$_GET['academic']='Pless'; 
+include( 'bibtexbrowser.php' ); 
+?>
+
+
+</body> 
+</html> 
+
