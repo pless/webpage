8ce774ed56aba21bbfd09d13cf6cb1cacd50cbc3
Robert Pless <pless@gwu.edu>
2022-01-09 16:35:28 -0500

    updates

---
+++ computationalPhotography/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd"> 
+<html> 
+  <head> 
+    <title> 
+    robert pless - computational photography
+    </title> 
+    <meta name="keywords" content="robert pless, computational geometry course notes"> 
+     <link rel="STYLESHEET" href="../mystyle.css" type="text/css">   
+  </head> 
+  <body> 
+<?php include("navbar.html") ?>
+<div class="content"> 
+<h3>CSCI 3907/6907, Computational Photography, Spring 2022</h3>
+
+This course meets once per week, Monday, 12:00 -- 2:30 pm.  Our first week we will be following University policies to meet online.  Information about course policies is available in the <a href="syllabus.html">course syllabus</a>, and this page will be updated to reflect the lecture topics, assignments and readings.
+
+<h5>Some reading</h5>
+Pop-culture reading on Computational Photography:
+<ol>
+  <li><a href="https://medium.com/@FriedmanArchives/computational-photography-1cdbccc3c2ef">Medium article about computational photography on high-end smartphones</a>.
+  <li><a href="https://www.americanscientist.org/article/computational-photography">American Scientist article on Computational Photography with more of a focus on the sensors</a>
+</ol>
+Some writings by researchers in Computational Photography:
+<ol>
+  <li><a href="https://medium.com/@aaronhertzmann/how-photography-became-an-art-form-7b74da777c63">Aaron Hertzman on Advances in Photography and Art</a>
+</ol>
+
+</div>
+</body>
+</html>
