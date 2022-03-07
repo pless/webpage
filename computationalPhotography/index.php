03e17297a98fd14166087ff67768b8b1ba2506e3
Robert Pless <pless@gwu.edu>
2022-03-07 14:58:15 -0500

    fixing things up

--- computationalPhotography/index.php
+++ computationalPhotography/index.php
@@ -24,8 +24,8 @@ sometimes do in-class demos in matlab, because it is what I know best,
 and because I think it makes the mathematical operations happening
 more clear.
 
-<h4>Project 2 is due 10am, Monday Febuary 14.</h4>
-Assignment <a href="project2.php">details here</a>, and please turn in your project via
+<h4>Project 3 is due 10am, Thursday, March 25.</h4>
+Assignment <a href="project3.php">details here</a>, and please turn in your project via
 <a href="https://forms.gle/DLKoBtUZXDMJHvUAA">this form</a></h4>
 
 <h4>Lectures and Slides</h5>
@@ -34,6 +34,7 @@ Assignment <a href="project2.php">details here</a>, and please turn in your proj
   <li><a href="slides/ComputationalPhotography2.ppt">Lecture 2 slides</a>
   <li><a href="slides/ComputationalPhotography3.ppt">Lecture 3 slides</a>, and <a href="imagesAsVectors.m">matlab code</a>.
   <li><a href="slides/ComputationalPhotography4.ppt">Lecture 4 slides</a> on Blind Deconvolution and coded aperture imaging, and <a href="deconvDemo.m">deconv Demo matlab code</a>, and the <a href="errf.m">errf.m</a> the ill fated error function that didn't really converge.
+  <li><a href="slides/ComputationalPhotography5.ppt">Lecture 5 slides</a>Flutter Shutter imaging and image based PCA,  <a href="lecture5.m">PCA demo matlab code</a>.
 </ol>
 <h4>Some reading</h5>
 Pop-culture reading on Computational Photography:
