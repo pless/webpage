2889d3f90a80f0f5b4eee966a231bf74b2955262
Robert Pless <pless@glint.local>
2023-09-13 17:34:38 -0400

    gates

--- GATES/tableScript.js
+++ GATES/tableScript.js
@@ -13,17 +13,29 @@ async function loadTable() {
   const displayText = document.getElementById("displayText");
   const table = document.getElementById("myTable");
 
+  let lastClickedCell = null;  // To keep track of the last clicked cell
+
   // Changed from mouseover to click
   table.addEventListener("click", async function(event) {
     const target = event.target;
+    // Remove the highlight from the last clicked cell, if any
+    if (lastClickedCell) {
+      lastClickedCell.classList.remove("highlight");
+    }
+  
+    // Highlight the current clicked cell
+    target.classList.add("highlight");
+    lastClickedCell = target;
     if (target.tagName === "TD") {
       const fileName = target.getAttribute("data-filename");
+      const summaryText = target.innerText; // Get the text content of the clicked cell
+
       try {
         const response = await fetch(`${fileName}`);
         if (response.ok) {
           let text = await response.text();
           text = text.replace(/\n/g, '<br>'); // Replace line feeds with <br>
-          displayText.innerHTML = text;
+          displayText.innerHTML = `<strong>Summary:</strong> ${summaryText}<hr>${text}`;
         } else {
           displayText.innerHTML = "Failed to fetch text for this cell.";
         }
